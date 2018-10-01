76cf7a98f29406147cd4fdc2b6bfb72399376e41
javagalbhanu <javagalbhanu@gmail.com>
2018-10-01 09:26:29 +0530

    Create DemoA.java

---
+++ DemoA.java
@@ -0,0 +1,22 @@
+package btm;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(Result.class)
+public class DemoA {
+	@Test
+	public void createCustomer(){
+		Reporter.log("Create a Customer",true);
+		Assert.fail();
+	}
+	
+	@Test
+	public void deleteCustomer(){
+		Reporter.log("Delete a Customer",true);
+	}
+	
+
+}
